014bb841691abf7fe43605d1b890cc58937dd2c2
Omnissiah <234093135+Zeta1313@users.noreply.github.com>
2026-05-12 11:00:53 -0700

    created components

---
+++ src/components/item.jsx
@@ -0,0 +1,7 @@
+export default function item(props) {
+    return (
+        <header className="header">
+            <img>{props.img}</img>
+        </header>
+    )
+}
---
+++ src/components/text.jsx
@@ -0,0 +1,7 @@
+export default function text(props) {
+    return (
+        <div>
+            <h1>{props.text}</h1>
+        </div>
+    )
+}
