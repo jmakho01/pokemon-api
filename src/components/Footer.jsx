c1e6d5b5b12dc56dfdfbce15f7ea9a42101b2b2d
Omnissiah <234093135+Zeta1313@users.noreply.github.com>
2026-05-12 11:06:44 -0700

    .

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-export default function Header() {
+export default function Footer() {
     return (
         <header className="footer">
             <h4>All Pokemon are the trademarked property of The Pokemon Company International</h4>
